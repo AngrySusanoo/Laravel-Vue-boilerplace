80a91e4817c7c8873d21aaf8e90121fe97c678be
AngrySusanoo <126177355+AngrySusanoo@users.noreply.github.com>
2023-03-06 20:02:44 -0500

    Update server.php

--- server.php
+++ server.php
@@ -10,6 +10,10 @@
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
+// Recently added this part.
+$uri_secondary = urldecode(
+    parse_url($_SERVCER['RESPONSE_URI'], PHP_URL_PATH)
+);
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
